e5ef36d854bf7745bb59aabdf72fa6c199ac32b6
deepikaaap <deepikapadmanaban95@gmail.com>
2019-10-25 14:45:00 +0200

    Cosine Annealing update

--- src/utils/cosine_annealing.py
+++ src/utils/cosine_annealing.py
@@ -1,30 +1,15 @@
 import math
-from keras.callbacks import Callback
-from keras import backend as K
 
+class CosineAnnealingScheduler():
 
-class CosineAnnealingScheduler(Callback):
-    """
-    This class 'CosineAnnealingScheduler' has been taken from a previously implemented code.
-    Source code reference - https://github.com/4uiiurz1/keras-cosine-annealing/
-    """
-
-    def __init__(self, T_max, eta_max, eta_min=0, verbose=0):
+    def __init__(self, T_max, eta_max, learning_rate, eta_min=0):
         super(CosineAnnealingScheduler, self).__init__()
         self.T_max = T_max
         self.eta_max = eta_max
         self.eta_min = eta_min
-        self.verbose = verbose
+        self.learning_rate = learning_rate
 
-    def on_epoch_begin(self, epoch, logs=None):
-        if not hasattr(self.model.optimizer, 'lr'):
-            raise ValueError('Optimizer must have a "lr" attribute.')
-        lr = self.eta_min + (self.eta_max - self.eta_min) * (1 + math.cos(math.pi * epoch / self.T_max)) / 2
-        K.set_value(self.model.optimizer.lr, lr)
-        if self.verbose > 0:
-            print('\nEpoch %05d: CosineAnnealingScheduler setting learning '
-                  'rate to %s.' % (epoch + 1, lr))
+    def find_current_learning_rate(self, iter):
+        lr = self.eta_min + (self.eta_max - self.eta_min) * (1 + math.cos(math.pi * iter / self.T_max)) / 2
+        return lr
 
-    def on_epoch_end(self, epoch, logs=None):
-        logs = logs or {}
-        logs['lr'] = K.get_value(self.model.optimizer.lr)
